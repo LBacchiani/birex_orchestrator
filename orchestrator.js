f9f2cfa3d8c61f0767826925b19ca57023357a61
LBacchiani <>
2022-03-03 11:32:09 +0100

    new threshold

--- orchestrator.js
+++ orchestrator.js
@@ -81,35 +81,73 @@ async function apply(specPath) {
     return created;
 }
 
+async function setSize() {
+  let sizes = [100, 1000, 10000, 20000, 30000, 40000, 50000]; //0,01mb, 1mb, 100mb, 400mb, 900mb, 1600mb, 2500mb
+  let i = 0
+  let direction = true
+  while(true) {
+    await sleep(60000 * 10);
+    let value = sizes[i]
+    await fetch("http://birex-collector:8080/birexcollector/actions/setSizes", {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({minSize: value, maxSize: value})
+    });
+    if(direction) {
+      i = i + 1;
+      if(i == sizes.length - 1) direction = false;
+    }
+    else {
+      i = i - 1;
+      if(i == 0) direction = true;
+    }
+  }
+}
+
+async function retrieveLatency() {
+  var latency = 'rate(istio_request_duration_milliseconds_sum{app="alerting",destination_workload="processor-' + zone + '"}[5m]) / rate(istio_requests_total{app="alerting",destination_workload="processor-' + zone + '"}[5m])'
+  await prom.instantQuery(latency).then((res) => {
+    const series = res.result;
+    series.filter(serie => !isNaN(serie.value.value)).forEach((serie) => await retrieveBytes(serie.value.value));
+  }).catch(console.error);
+}
+
+async function retrieveBytes(latency) {
+  var bytes = 'rate(istio_request_bytes_sum{app="alerting", destination_workload="processor-' + zone + '"}[5m])/rate(istio_requests_total{app="alerting", destination_workload="processor-' + zone + '"}[5m])'
+  await prom.instantQuery(latency).then((res) => {
+    const series = res.result;
+    series.filter(serie => !isNaN(serie.value.value)).forEach((serie) => {
+      bytes = serie.value.value
+      console.log(zone + ": (" + latency + "," + bytes + ")")
+      if(latency > 1000 * 5 || bytes > 100 * 1000000) { // latency 5s & sent bytes 100mb threshold
+         if(zone != "edge") {
+           zone = "edge"
+           apply(processor_edge).catch(err => { console.log(JSON.stringify(err))});
+          } else {
+            zone = "cloud"
+            apply(processor_cloud).catch(err => { console.log(JSON.stringify(err))});
+          }
+      }
+    });
+  }).catch(console.error);
+}
+
 async function monitoring() {
 
   while (true) {
     console.log(`\nI will sleep for 5 minutes\n`)
-    await sleep(60000 * 5);
-    var latency = 'rate(istio_request_duration_milliseconds_sum{app="alerting",destination_workload="processor-' + zone + '"}[5m]) / rate(istio_requests_total{app="alerting",destination_workload="processor-' + zone + '"}[5m])'
-
+    await sleep(60000 * 2)
     console.log(`Executing query:     ${latency}`)
-
-    await prom.instantQuery(latency).then((res) => {
-      const series = res.result;
-      series.filter(serie => !isNaN(serie.value.value)).forEach((serie) => {
-        console.log("Serie:", serie.metric.toString());
-        console.log("Time:", serie.value.time);
-        console.log("Value:", serie.value.value);
-        if(serie.value.value > 80) {
-          if(zone == "cloud") zone = "edge"
-          else zone = "cloud"
-          apply((zone == "cloud") ? processor_cloud : processor_edge).catch(err => { console.log(JSON.stringify(err))});
-          safeDelete()
-          console.log("Processor zone: " + zone);
-        }
-      });
-    }).catch(console.error);
+    await retrieveLatency()
   }
 }
 
 
-monitoring();
+monitoring()
+setSizes()
 
 console.log("\nOrchestrator started\n")
 
