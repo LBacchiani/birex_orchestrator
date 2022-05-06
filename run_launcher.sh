88903d70cd54d909b1f2f01b895c89da752ee66f
LBacchiani <>
2022-05-06 10:27:09 +0100

    fix launcher

--- run_launcher.sh
+++ run_launcher.sh
@@ -4,7 +4,7 @@ do
         kubectl apply -k edge-server/servizi-luca/standalone/
         sleep 30
         kubectl apply -f orchestrator.yaml
-        sleep 20m
+        sleep 11m
         kubectl logs pod orchestrator >> orchestrator.log
         kubectl delete -k edge-server/servizi-luca/standalone/
         kubectl delete -f edge-server/servizi-luca/standalone/processor-edge.yaml
