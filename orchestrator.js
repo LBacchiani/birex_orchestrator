d25252676f7a1f1e77f351a20dacdf71fb4e062e
LBacchiani <>
2022-05-10 17:38:09 +0100

    fix?

--- orchestrator.js
+++ orchestrator.js
@@ -12,7 +12,7 @@ const kc = new k8s.KubeConfig();
 kc.loadFromDefault();
 const k8sApi = kc.makeApiClient(k8s.CoreV1Api);
 
-const endpoint = "http://10.244.1.4:9090";
+const endpoint = "http://10.244.2.3:9090";
 const baseURL = "/api/v1" // default value
 const path = '/usr/src/app/edge-server/servizi-luca/standalone/'
 const processor_cloud = path + 'processor-cloud.yaml'
@@ -32,31 +32,16 @@ const prom = new PrometheusDriver({
   baseURL
 });
 
-function sleep(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
-}
+function sleep(ms) {return new Promise(resolve => setTimeout(resolve, ms));}
 
-async function safeDelete(z) {
-  var deleted = false
-  while (!deleted) {
-    await sleep(50)
-    await prom.instantQuery('up{kubernetes_pod_name="processor-' + zone + '"}').then((res) => {
-      const series = res.result;
-      series.forEach((serie) => {
-        if (!isNaN(serie.value.value)) {
-          k8sApi.deleteNamespacedPod((zone == "cloud") ?
-            'processor-cloud' :
-            'processor-edge', 'default', true)
-            .catch(err => { console.log(JSON.stringify(err)) });
-          deleted = true;
-        }
-      });
-    }).catch(console.error);
-  }
+function safeDelete(z) {
+  k8sApi.deleteNamespacedPod((zone == "cloud") ? 'processor-cloud' :'processor-edge', 'default', true)
+  .catch(err => { console.log(JSON.stringify(err))});
   zone = z
 }
 
-async function apply(specPath) {
+
+async function apply(specPath, zone) {
   const client = k8s.KubernetesObjectApi.makeApiClient(kc);
   const fsReadFileP = promisify(fs.readFile);
   const specString = await fsReadFileP(specPath, 'utf8');
@@ -86,6 +71,7 @@ async function apply(specPath) {
   }
   const stop = new Date()
   console.log("Time to deploy: " + (stop - start) + "ms")
+  safeDelete(zone)
   return created;
 }
 
@@ -103,25 +89,15 @@ async function setSize(inc = true) {
 
 async function retrieveLatency() {
   var latencyQuery = 'rate(istio_request_duration_milliseconds_sum{app="alerting"}[30s]) / rate(istio_requests_total{app="alerting"}[30s])'
-  //var latencyQuery = 'rate(istio_request_duration_milliseconds_sum{app="alerting",destination_workload="processor-' + zone + '"}[10s]) / rate(istio_requests_total{app="alerting",destination_workload="processor-' + zone + '"}[10s])'
   await prom.instantQuery(latencyQuery).then(async (res) => {
     const serie = res.result.filter(serie => !isNaN(serie.value.value))[0]
     await retrieveBytes(serie.value.value)
   }).catch(console.error);
 }
 
-function moveToEdge() {
-  //zone = "edge"
-  apply(processor_edge).catch(err => { console.log(JSON.stringify(err)) })
-  safeDelete("edge")
-}
+function moveToEdge() {apply(processor_edge, "edge").catch(err => { console.log(JSON.stringify(err))})}
 
-function moveToCloud() {
-  //zone = "cloud"
-  apply(processor_cloud).catch(err => { console.log(JSON.stringify(err)) })
-  safeDelete("cloud")
-}
-
+function moveToCloud() {apply(processor_cloud, "cloud").catch(err => { console.log(JSON.stringify(err))})}
 
 async function retrieveBytes(latency) {
   var bytesQuery = 'rate(istio_response_bytes_sum{app="collector", source_canonical_service="unknown"}[30s]) / rate(istio_requests_total{app="collector", source_canonical_service="unknown"}[30s])'
@@ -135,22 +111,8 @@ async function retrieveBytes(latency) {
   }).catch(console.error);
 }
 
-async function retrieveLatencyWithoutBytes() {
-  let bytes = sizes[index % sizes.length]
-  times = times + 1
-  //var latencyQuery = 'rate(istio_request_duration_milliseconds_sum{app="alerting",destination_workload="processor-' + zone + '"}[30s]) / rate(istio_requests_total{app="alerting",destination_workload="processor-' + zone + '"}[30s])'
-  console.log("querying zone: " + zone)
-  await prom.instantQuery(latencyQuery).then(async (res) => {
-    const latency = res.result.filter(serie => !isNaN(serie.value.value))[0].value.value
-    console.log(zone + ": (" + latency + "," + bytes * bytes * 3500 + ")")
-    if (times % 16 == 0) console.log("-------")
-    if (zone == "cloud" && latency > 1000 * 1.8 && times % 16 != 0) moveToEdge()
-    else if ((zone == "edge" && latency < 1000 * 1 && bytes < 65 * 65 * 3500) || times % 16 == 0) moveToCloud()
-  }).catch(console.error);
-}
-
 async function monitoring() {
-  //console.log(`Start monitoring...`)
+  console.log(`Start monitoring...`)
   let i = 0
   setSize(false)
   await sleep(60000)
@@ -159,7 +121,6 @@ async function monitoring() {
     await sleep(30000)
     i = i + 1
     await retrieveLatency()
-    //await retrieveLatencyWithoutBytes()
   }
 }
 
